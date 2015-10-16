674936caf0ef6057556baeedd4d4927e5e171de0
Michael Christensen <mc13818@newblue4.cm.cluster>
2015-10-16 15:07:47 +0100

    added environment script for submission

---
+++ simulation/env.sh
@@ -0,0 +1,9 @@
+# Load Python module
+module load languages/python-2.7.6
+
+# Load icc compiler
+module load languages/intel-compiler-16
+
+# Load Tau
+module add tools/gnu_builds/tau-2.23.1-openmp
+
